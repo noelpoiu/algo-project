7c44ee1bd4997ae275fa2dc673745ec2aefaf015
Noel Felföldi <NOEL.FELFOLDI@gmail.com>
2023-09-21 18:08:40 +0200

    ok

---
+++ instr.py
@@ -0,0 +1,39 @@
+text1 = "Welcome to the Health status detection program!"
+
+
+
+
+
+
+text2 = "This application allows you to use the Rufier test to make an initial diagnosis of your health.\
+The Rufier test is a set of physichal exercises designed to assess your cardiac performance during physical exertion.˛\
+The subject lies in the supine position for 5 minutes and has their pulse rate measured for 15 seconds;\
+When the exercise ends, the subject lies down and their pulse rate measured again for the first 15 seconds\
+and then for the last 15 seconds of the first minute of the recovery period.\
+Important! If you feel unwell during the test (dizziness,\
+tinnitus, shortness of breath, ect.), stop the test and consult a physicain."
+
+
+
+
+txt_title = "Enter Your full name:"
+
+text2 = "Full years:"
+
+text3 = "Lie on your back  and take your pulse for 15 seconds. Click the ''Start first test'' button to start the timer."
+text4 = "Write down the result in the appropriate field."
+buttontext ="Start first test"
+
+text5 = "Perforn 30 squats in 45 seconds. To do this, click the ''Start doing squats'' button to start the squat counter."
+buttontext2  = "Start doing squats"
+text6 = "Lie on your back and take your pulse for the first 15 seconds of the minute, then for the last 15 bseconds of the minute.minPress the "Start final test" button to  start the timer.\nThe seconds thatshould be measured are indicated  in green and the minutes that should not be measured are indicated in black. Write down the results in the appropriate fields."
+buttontext3 = "Start the final test"
+
+
+
+
+
+
+text7 = "Roufler index:"
+text8 = "Cardiac performance:"
+text9 = "there is no data for this age"
